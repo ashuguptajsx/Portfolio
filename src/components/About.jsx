215c3204ded25f84518d852d39f5cb8f26bdb4ef
ashu-1403 <129203568+ashu-1403@users.noreply.github.com>
2024-07-24 12:14:20 +0530

    comit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,11 +5,11 @@ const About = () => {
   return (
     <div className="border-b border-neutral-900 pb-10">
       <div className="flex items-center">
-        <h1 className="bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 bg-clip-text text-transparent ml-10 text-xl lg:text-4xl ml-16 lg:ml-64 font-In-ter">
+        <h1 className="bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 bg-clip-text text-transparent ml-10 text-xl lg:text-4xl ml-16 lg:ml-52 font-In-ter">
           About Me
         </h1>
        
-        <div className="flex-grow border-t-4 border-white mr-14 lg:mr-80"></div>
+        <div className="flex-grow border-t-4 border-white mr-14 lg:mr-72"></div>
      
       </div>
       <div className="flex flex-row gap-4">
